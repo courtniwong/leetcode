ec3b414c87f8f8cf3e9a363dc7e507f986f75191
courtniwong <courtniwong@gmail.com>
2017-02-28 23:02:49 -0800

    adding invert binary tree

---
+++ invert_binary_tree.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null){
+            return root;
+        } 
+        
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
